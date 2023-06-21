88de27ba5907b3e9027c9426e3c2f98d41d9e6da
Frank Jover <joverjlj@gmail.com>
2023-06-21 16:47:55 +0200

    readme

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -44,12 +44,12 @@ export default function ChatPage({}) {
 
       <div className="w-[65rem] h-[40rem] bg-none rounded-tr-3xl rounded-br-3xl flex shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
         <div className="w-96  border-gray-200 bg-white rounded-tr-3xl rounded-br-3xl">
-          <div className="shadow-[0px_3px_5px_0px_#A0AEC0] flex items-center h-28 justify-center ">
+          <div className="flex items-center h-28 justify-center ">
             {isClicked ? "SETTINGS" : "Members"}
           </div>
         </div>
         <div className=" w-[42rem] ml-2 rounded-tr-3xl rounded-br-3xl">
-          <div className="shadow-[0px_3px_0px_0px_#1a202c] flex items-center h-28 justify-end pr-4 rounded-tl-3xl rounded-tr-3xl bg-white">
+          <div className="flex items-center h-28 justify-end pr-4 rounded-tl-3xl rounded-tr-3xl bg-white">
             <Image
               src="/chat-logo.png"
               alt="sonup-logo"
@@ -58,9 +58,7 @@ export default function ChatPage({}) {
               className="mr-3"
             />
           </div>
-          <div className="h-[33rem] rounded-br-3xl rounded-bl-3xl bg-white">
-            test
-          </div>
+          <div className="h-[33rem] rounded-br-3xl rounded-bl-3xl bg-white"></div>
         </div>
       </div>
     </Container>
